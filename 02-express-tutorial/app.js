74af80ebb00ea4cfe475ab740b4beb066f7e3704
Jairus Macatangay <jai.macatangay@gmail.com>
2021-08-19 10:42:58 +0800

    initial commit

---
+++ 02-express-tutorial/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const people = require('./routes/people')
+const auth = require('./routes/auth')
+
+// static assets
+app.use(express.static('./methods-public'))
+
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+
+// parse json
+app.use(express.json())
+
+app.use('/login', auth)
+
+app.use('/api/people', people)
+
+app.listen(5000, () => {
+    console.log('server is listening on port 5000...');
+})
